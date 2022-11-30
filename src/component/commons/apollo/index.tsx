318d9efbfa3a6e252559095deb8661f5c0f097f7
mibu119 <horang296@gmail.com>
2022-11-30 19:21:46 +0900

    [2022.11.30] 22일차 클래스 퀴즈

--- src/component/commons/apollo/index.tsx
+++ src/component/commons/apollo/index.tsx
@@ -5,14 +5,18 @@ import {
   ApolloLink,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../commons/stores";
 
 interface IPropsApolloSetting {
   children: JSX.Element;
 }
 
 export default function ApolloSetting(props: IPropsApolloSetting) {
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
   const uploadLink = createUploadLink({
     uri: "http://backend10.codebootcamp.co.kr/graphql",
+    headers: { Authorization: `Bearer ${accessToken}` },
   });
 
   const client = new ApolloClient({
